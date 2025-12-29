dec4f1686b5b5ff88767cd8efc45dc8a1bf7f65e
aliciajocelyn <alisiahaya.college@gmail.com>
2025-12-29 14:14:11 +0700

    [New] Add topic name mapping dictionary

---
+++ src/dictionary/topic_name_mapping.py
@@ -0,0 +1,17 @@
+POSITIVE_TOPIC_MAPPING = {
+    "-1_event_teman_organisasi_kampus": "Event dan Teman dari Organisasi Kampus",
+    "0_teman_kampus_dosen_materi": "Lingkungan Kampus dan Dosen",
+    "1_bertemu_teman_orang_koneksi": "Pertemanan, Relasi dan Koneksi",
+    "2_bareng_belajar_teman_ujian": "Kegiatan dan Belajar Bersama Teman", 
+    "3_program_mahasiswa_magang_enrichment": "Program Magang dan Enrichment",
+    "4_kegiatan_program_acara_seru": "Kegiatan dan Acara Kampus",
+}
+
+NEGATIVE_TOPIC_MAPPING = {
+    "-1_fasilitas_kampus_dosen_kuliah": "Kualitas Fasilitas Kampus dan Pengajaran Dosen",
+    "0_kampus_wifi_fasilitas_toilet": "Fasilitas Kampus: Wifi, Toilet, dan Infrastruktur",
+    "1_dosen_materi_mahasiswa_mengajar": "Relevansi Materi Kuliah dan Metode Pengajaran Dosen",
+    "2_toilet_tissue_fasilitas_tangan": "Kebersihan dan Kelengkapan Fasilitas Toilet Kampus",
+    "3_kuliah_mata_pagi_jam": "Jadwal Kuliah Pagi dan Relevansi Mata Kuliah",
+    "4_tugas_ubah_menumpuk_bersamaan": "Beban Tugas Mahasiswa"
+}
