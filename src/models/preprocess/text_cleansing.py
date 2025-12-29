a4857424ce060ecff7c860bc82feaf2be7a9cf65
aliciajocelyn <alisiahaya.college@gmail.com>
2025-12-29 14:13:54 +0700

    [New] Add text_cleansing module

--- src/models/preprocess/text_cleansing.py
+++ src/models/preprocess/text_cleansing.py
@@ -1,9 +1,27 @@
 import re
+from nlp_id.tokenizer import Tokenizer
+from nlp_id.stopword import StopWord
+
+from src.dictionary.exclude_words import exclude_stopwords
+
+negation_phrases = [
+    'sangat tidak menyukai', 'tidak menyukai', 'sangat tidak suka',
+    'tidak suka', 'kurang suka', 'kurang menyukai', 'ga suka',
+    'gak suka', 'ga menyukai', 'gak menyukai', 'gada'
+]
+
+positive_phrases = ['suka', 'sangat suka', 'menyukai', 'sangat menyukai']
 
 class TextCleansing:
-    def __init__(self, text, norm_dict=None):
+    def __init__(self, text, norm_dict=None, exclude_stopwords=None, add_stopwords=['nya', 'ya', 'nih']):
         self.text = text
         self.norm_dict = norm_dict if norm_dict else {}
+        self.exclude_stopwords = exclude_stopwords if exclude_stopwords else []
+
+        self.tokenizer = Tokenizer()
+        self.stopword_list = self.stopword.get_stopword()
+        self.stopword_list.append(add_stopwords)
+        
 
     def correct_typos(self, text):
         for typo, correction in self.norm_dict.items():
@@ -34,6 +52,26 @@ class TextCleansing:
             self.split_nya(word.strip()) for word in words
         ]
         return ' '.join(processed_words)
+    
+    def clean_text_topic(self, negation=False): 
+        if negation: 
+            for phrase in negation_phrases:
+                self.text = self.text.replace(phrase, '')
+        else:
+            for phrase in positive_phrases:
+                self.text = self.text.replace(phrase, '')
+        text = re.sub(r"[^a-zA-Z\s']", ' ', text)
+        text = re.sub(r'\bga\b', '', text)
+        return re.sub(r'\s+', ' ', text).strip()
+    
+    def text_preprocessing_topic(self):
+        tokens = self.tokenizer.tokenize(self.text)
+        processed_tokens = [
+            word if (word not in self.exclude_stopwords) else word
+            for word in tokens 
+            if word not in self.stopword_list or word in self.exclude_stopwords
+        ]
+        return " ".join(re.sub(r'\s+', ' ', processed_tokens).strip())
 
     def clean(self):
         text = self.text.lower()
